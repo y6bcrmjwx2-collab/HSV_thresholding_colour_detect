53d97e2bfd21540983ab97122d07ff5202d2fb1b
Maile725 <maiqihe@gmail.com>
2026-02-27 11:57:01 +0800

    completed raw file embedding

--- main/HSV_thresholding_colour_detect.c
+++ main/HSV_thresholding_colour_detect.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <math.h>
+#include "esp_log.h"
 
+extern const uint16_t rgb565_start[] asm("_binary_lutino_brightlight_rgb565_raw_start");
+extern const size_t RGB565_size asm("_binary_lutino_brightlight_rgb565_raw_size");
+
+size_t pixel_count = RGB565_size / 2;
+const uint16_t *pixels = rgb565_start;
+
+const char *TAG = "image_processing";
 void app_main(void)
 {
 
+    printf("Image loaded \n");
+    printf("Size: %d, %d pixels\n", RGB565_size, pixel_count);
 }
