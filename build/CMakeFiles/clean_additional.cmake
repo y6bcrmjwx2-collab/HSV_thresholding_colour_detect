4e74ad0765666af8aaac9dbf4531be456bd0cddb
Maile725 <maiqihe@gmail.com>
2026-02-28 12:22:16 +0800

    fixed bugs in the code, flash error

--- build/CMakeFiles/clean_additional.cmake
+++ build/CMakeFiles/clean_additional.cmake
@@ -18,6 +18,7 @@ if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
   "flasher_args.json"
   "ldgen_libraries"
   "ldgen_libraries.in"
+  "lutino_brightlight_rgb565.raw.S"
   "project_elf_src_esp32s3.c"
   "x509_crt_bundle.S"
   )
